6f2a437b013c861043518f541e8afb890b07f15c
B.Raj Bhujel <work.brajbhujel@gmail.com>
2024-06-19 21:43:00 +0545

    Raj

--- src/components/SetupSection.js
+++ src/components/SetupSection.js
@@ -16,34 +16,16 @@ const SetupSection = () => {
       <h2>
         Share your setup with <span>#FuniroFurniture</span>
       </h2>
-      <div className="grid">
-        <div className="item item1">
-          <img src={Image1} alt="Setup 1" />
-        </div>
-        <div className="item item2">
-          <img src={Image2} alt="Setup 2" />
-        </div>
-        <div className="item item3">
-          <img src={Image3} alt="Setup 3" />
-        </div>
-        <div className="item item4">
-          <img src={Image4} alt="Setup 4" />
-        </div>
-        <div className="item item5">
-          <img src={Image5} alt="Setup 5" />
-        </div>
-        <div className="item item6">
-          <img src={Image6} alt="Setup 6" />
-        </div>
-        <div className="item item7">
-          <img src={Image7} alt="Setup 7" />
-        </div>
-        <div className="item item8">
-          <img src={Image8} alt="Setup 8" />
-        </div>
-        <div className="item item9">
-          <img src={Image9} alt="Setup 9" />
-        </div>
+      <div className="parent">
+        <img src={Image1} alt="Setup 1" className="div1" />
+        <img src={Image2} alt="Setup 2" className="div2" />
+        <img src={Image3} alt="Setup 3" className="div3" />
+        <img src={Image4} alt="Setup 4" className="div4" />
+        <img src={Image5} alt="Setup 5" className="div5" />
+        <img src={Image6} alt="Setup 6" className="div6" />
+        <img src={Image7} alt="Setup 7" className="div7" />
+        <img src={Image8} alt="Setup 8" className="div8" />
+        <img src={Image9} alt="Setup 9" className="div9" />
       </div>
     </div>
   );
